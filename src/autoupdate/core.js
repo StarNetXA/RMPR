103bc2c173af6391c2bded4726d560eeff71ddd4
Tianpao <tianpaoweb@outlook.com>
2025-01-31 17:27:12 +0800

    add：更新提醒

--- src/autoupdate/core.js
+++ src/autoupdate/core.js
@@ -5,7 +5,10 @@ const i18n = i18ns()
 
 export async function checkupdate(){
 try{
-await got.get("https://rmpr.starnetx.top/api/latest",{headers:{"User-Agent":`RMPR/${config.version}`}})
+const latestv = JSON.parse((await got.get("https://rmpr.starnetx.top/api/latest.json",{headers:{"User-Agent":`RMPR/${config.version}`}})).body).version
+if (latestv !== config.version){
+console.log(`\x1B[33m${i18n.check_update_tips}${latestv}`)
+}
 }catch(e){
     console.log(`\x1B[31mError：${i18n.check_update_error}\x1B[0m`)
 }
